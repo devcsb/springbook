5df3f36c224f2c87dfea00953077d02b1d0efd98
devcsb <devcsb119@gmail.com>
2022-05-15 15:00:07 +0900

    9.5 배포 자동화 구성
    scripts 폴더에 변경된 deploy.sh 추가,
    .travis.yml 수정(모든파일 압축에서 필요한 파일만 압축해서 넘겨주도록)
    appspec.yml 수정.

---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=springbook
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then # -z 조건 : 문자열의 길이가 0이면 true 반환
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/$JAR_NAME 2>&1 &
+    #nohup 실행 시 CodeDeploy는 무한 대기한다. 이 이슈를 해결하기 위해 nohup.out 파일을 표준 입출력용으로 별도로 사용한다.
+    #이렇게 하지 않으면 nohup.out 파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력된다.
+    #nohup이 끝나기 전까진 CodeDeploy도 끝나지 않으니 꼭 이렇게 해야만 한다.
